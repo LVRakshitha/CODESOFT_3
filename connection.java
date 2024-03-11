1c7966ab4340ab865b88c5fb2fd53fb512cffa69
L V RAKSHITHA <121225705+LVRakshitha@users.noreply.github.com>
2024-03-11 14:44:29 +0530

    Create connection.java

---
+++ connection.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+public class conn {
+	
+	Connection c;
+	Statement s;
+	
+	public  conn() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			c = DriverManager.getConnection("jdbc:mysql:///student_mgm_stm","root","");
+			s = c.createStatement();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+}
